bec4d10fc8b4c405cc712aeb1a1d4d5640c93d80
Lucia Paterlini <luciam.paterlini@gmail.com>
2023-10-28 17:26:35 +0200

    se soluciono el problema del navbar, quedo responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,36 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
-const NavBar = ({ cambiarLogIn }) => {
+const NavBar = () => {
 return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-                <div className="col-md-3 mb-2 mb-md-0">
+        <div className="container">
+            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom w-100">
+                <div className="col-lg-3 mb-2 mb-md-0">
                     <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
-                    <svg
-                        className="bi"
-                        width="40"
-                        height="32"
-                        role="img"
-                        aria-label="Bootstrap">
-                        <use xlink:href="#bootstrap"></use>
-                    </svg>
+                    <span>RollingGym</span>
                     </a>
                 </div>
 
                 <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                     <li>
-                        <Link href="#" className="nav-link px-2 link-secondary"> Home</Link>
+                        <NavLink className="nav-link px-2 link-secondary"> Home</NavLink>
                     </li>
                     <li>
-                        <Link href="#" className="nav-link px-2">Programas</Link>
+                        <NavLink className="nav-link px-2">Programas</NavLink>
                     </li>
                     <li>
-                        <Link href="#" className="nav-link px-2">Turnos</Link>
+                        <NavLink className="nav-link px-2">Turnos</NavLink>
                     </li>
                     <li>
-                        <Link href="#" className="nav-link px-2">Nuestro Equipo</Link>
+                        <NavLink className="nav-link px-2">Nuestro Equipo</NavLink>
                     </li>
                     <li>
-                        <Link href="#" className="nav-link px-2">About</Link>
+                        <NavLink className="nav-link px-2">About</NavLink>
                     </li>
                 </ul>
 
-                <div className="col-md-3 text-end">
+                <div className="col-lg-3 text-end">
                     <button type="button" className="btn btn-outline-primary me-2">
                     Login
                     </button>
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,19 @@ import './index.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import "font-awesome/css/font-awesome.css"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:  <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
