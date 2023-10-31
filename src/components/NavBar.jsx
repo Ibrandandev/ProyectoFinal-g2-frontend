588db1192c232eef9c73ddefa748c521d483409e
Lucia Paterlini <luciam.paterlini@gmail.com>
2023-10-31 21:43:16 +0100

    estilo navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,37 +6,37 @@ const NavBar = () => {
 return (
     <nav className="navbar navbar-expand-lg">
         <div className="container">
-            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom w-100">
-                <div className="col-lg-3 mb-2 mb-md-0">
-                    <Link className="navbar-brand" to="/">RollingGym</Link>
+            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-4 mb-2 border-bottom w-100">
+                <div className="col-lg-2 mb-2 mb-md-0">
+                    <Link className="navbar-brand text-white" to="/">RollingGym</Link>
                     {/* <a href="/" className="d-inline-flex link-body-emphasis text-decoration-none">
                     <span>RollingGym</span>
                     </a> */}
                 </div>
 
-                <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <ul className="navbar-nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                     <li className="nav-item">
-                        <NavLink className="nav-link px-2" to="/"> Home</NavLink>
+                        <NavLink className="nav-link px-3" to="/"> Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-2" to="/programas">Programas</NavLink>
+                        <NavLink className="nav-link px-3" to="/programas">Programas</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-2" to="/turno">Turnos</NavLink>
+                        <NavLink className="nav-link px-3" to="/turno">Turnos</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-2" to="/nuestroEquipo">Nuestro Equipo</NavLink>
+                        <NavLink className="nav-link px-3" to="/nuestroEquipo">Nuestro Equipo</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-2" to="/aboutUs">About</NavLink>
+                        <NavLink className="nav-link px-3" to="/aboutUs">About</NavLink>
                     </li>
                 </ul>
 
                 <div className="col-lg-3 text-end">
-                    <button type="button" className="btn btn-outline-primary me-2 btn-login">
+                    <button type="button" className="btn  me-2 btn-login">
                     Login
                     </button>
-                    <button type="button" className="btn btn-primary btn-style">
+                    <button type="button" className="btn  btn-style">
                     Sign-up
                     </button>
                 </div>
