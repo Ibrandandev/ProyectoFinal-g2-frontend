349f2fa1a8169b93077b3b6047e67aa7ea1d5642
Lucia Paterlini <luciam.paterlini@gmail.com>
2023-11-03 18:49:35 +0100

    agregue las fuentes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,9 @@ const NavBar = () => {
 return (
     <nav className="navbar navbar-expand-lg">
         <div className="container">
-            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-4 mb-2 border-bottom w-100">
-                <div className="col-lg-2 mb-2 mb-md-0">
-                    <Link className="navbar-brand logo text-white" to="/"><i class="fa fa-ravelry" aria-hidden="true"></i> RollingGym</Link>
+            <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-xl-between py-4 mb-2 border-bottom w-100">
+                <div className=" col-auto mb-2 mb-md-0">
+                    <Link className="navbar-brand logo text-white fs-4" to="/"><i class="fa fa-ravelry" aria-hidden="true"></i> RollingGym</Link>
                 </div>
 
                 <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -16,16 +16,16 @@ return (
                         <NavLink className="nav-link px-3" to="/"> Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-3" to="/programas">Programas</NavLink>
+                        <NavLink className="nav-link px-3" to="/servicios">Servicios</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-3" to="/turno">Turnos</NavLink>
+                        <NavLink className="nav-link px-3" to="/planes">Planes</NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link px-3" to="/nuestroEquipo">Nuestro Equipo</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link px-3" to="/aboutUs">About</NavLink>
+                        <NavLink className="nav-link px-3" to="/contacto">Contacto</NavLink>
                     </li>
                     {/* {user.rol === "ADMIN_ROLE" && ( aqui hay q poner la funcion q permite entrar a admin */}
                     <li className="nav-item">
@@ -35,13 +35,11 @@ return (
                     {/* )} */}
                 </ul>
 
-                <div className="col-lg-3 text-end">
-                    <button type="button" className="btn  me-2 btn-login">
-                    Login
-                    </button>
-                    <button type="button" className="btn  btn-style">
-                    Sign-up
-                    </button>
+                <div className="col-auto text-end">
+                    <div>
+                        <button type="button" className="btn me-2 btn-login">Login</button>
+                        <button type="button" className="btn  btn-style">Sign-up</button>
+                    </div>
                 </div>
             </header>
         </div>
