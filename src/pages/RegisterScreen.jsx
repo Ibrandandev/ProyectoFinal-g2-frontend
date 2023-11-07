17bb925d6c884968a9ab724fbb320e91aee0f1bc
Gonza Garcia <victorgonzalogarcia9@gmail.com>
2023-11-07 16:23:40 -0300

    agregando estilo de css para registro

--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { crearUsuario } from "../helpers/UsuariosApi";
+import "../css/RegisterScreen.css"
 
 function RegisterScreen() {
     const [regData, setRegData] = useState({
@@ -31,63 +32,81 @@ function RegisterScreen() {
     return (
         <>
         <div>
-            <h2>Registro</h2>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Nombre:</label>
-                <input 
-                type="text"
-                id="nombre"
-                name="nombre"
-                value={regData.nombre}
-                onChange={handleChange}
-                required
-                />
-                <label htmlFor="name">Apellido:</label>
-                <input 
-                type="text"
-                id="apellido"
-                name="apellido"
-                value={regData.apellido}
-                onChange={handleChange}
-                required
-                />
-                <label htmlFor="email">Email:</label>
-                <input 
-                type="email"
-                id="email"
-                name="email"
-                value={regData.email}
-                onChange={handleChange}
-                required
-                />
-                <label htmlFor="">Telefono:</label>
-                <input
-                type="text"
-                id="telefono"
-                value={regData.telefono}
-                onChange={handleChange}
-                required/>
-                <label htmlFor="">Planes:</label>
-                <select
-                className="form-select"
-                aria-label="Default select example"
-                id="planContratado"
-                value={regData.planContratado}
-                onChange={handleChange}>
-                <option value="FULL">Plan Full</option>
-                <option value="MUSCULACION">Plan Musculacion</option>
-                <option value="CLASES">Plan Solo clases</option>
-                </select>
-                <label htmlFor="password">Contraseña:</label>
-                <input 
-                type="password"
-                id="password"
-                name="password"
-                value={regData.password}
-                onChange={handleChange}
-                required
-                />
-                <button type="submit">Sign up</button>
+                <div>
+                    <h2>Registro</h2>
+                </div>
+                <div>
+                    <label htmlFor="name">Nombre:</label>
+                    <input 
+                    type="text" 
+                    id="nombre" 
+                    name="nombre" 
+                    value={regData.nombre} 
+                    onChange={handleChange} 
+                    required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="name">Apellido:</label>
+                    <input 
+                    type="text" 
+                    id="apellido" 
+                    name="apellido" 
+                    value={regData.apellido} 
+                    onChange={handleChange} 
+                    required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    value={regData.email} 
+                    onChange={handleChange} 
+                    required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="">Telefono:</label>
+                    <input 
+                    type="text" 
+                    id="telefono" 
+                    value={regData.telefono} 
+                    onChange={handleChange} 
+                    required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="">Planes:</label>
+                    <select 
+                    className="form-select" 
+                    aria-label="Default select example" 
+                    id="planContratado" 
+                    value={regData.planContratado} 
+                    onChange={handleChange}
+                    >
+                        <option value="FULL">Plan Full</option>
+                        <option value="MUSCULACION">Plan Musculacion</option>
+                        <option value="CLASES">Plan Solo clases</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="password">Contraseña:</label>
+                    <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    value={regData.password} 
+                    onChange={handleChange} 
+                    required
+                    />
+                </div>
+                <div>
+                    <button type="submit">Sign up</button>
+                </div>
             </form>
         </div>
         </>
