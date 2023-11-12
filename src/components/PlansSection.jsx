cd2ca9c3e36fd258c3c922c9cbaeaf4b2723981f
Gonza Garcia <victorgonzalogarcia9@gmail.com>
2023-11-12 17:04:28 -0300

    estilo de cards planes

--- src/components/PlansSection.jsx
+++ src/components/PlansSection.jsx
@@ -1,65 +1,73 @@
+import "../css/home-section.css"
+
 const PlansSection = () => {
   return (
-    <section className="container">
+    <section className="container mt-5">
       <h2 className="text-center">Plans</h2>
-      <div className="row justify-content-center gap-2">
-        <div className="card col-3">
-          <div className="card-body">
-            <h5 className="card-title fs-3">Plan Total</h5>
-            <hr />
-            <p className="card-text fs-5 text-center py-2">
-              $<span> 10.000</span>/mes
-            </p>
+      <div className="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title fs-3">Plan Total</h5>
+                <hr />
+                <p className="card-text fs-5 text-center py-2">
+                  $<span>10,000</span>/mes
+                </p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+              </ul>
+              <div className="card-body">
+                <a href="#" className="btn btn-primary w-75 mx-auto d-block">
+                  Asociate
+                </a>
+              </div>
+            </div>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="btn btn-primary w-75 mx-auto d-block">
-              Asociate
-            </a>
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title fs-3">Plan Total</h5>
+                <hr />
+                <p className="card-text fs-5 text-center py-2">
+                  $<span>10,000</span>/mes
+                </p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+              </ul>
+              <div className="card-body">
+                <a href="#" className="btn btn-primary w-75 mx-auto d-block">
+                  Asociate
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card col-3">
-          <div className="card-body">
-            <h5 className="card-title fs-3">Plan Total</h5>
-            <hr />
-            <p className="card-text fs-5 text-center py-2">
-              $<span> 10.000</span>/mes
-            </p>
+          <div className="col">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title fs-3">Plan Total</h5>
+                <hr />
+                <p className="card-text fs-5 text-center py-2">
+                  $<span>10,000</span>/mes
+                </p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+              </ul>
+              <div className="card-body">
+                <a href="#" className="btn btn-primary w-75 mx-auto d-block">
+                  Asociate
+                </a>
+              </div>
+            </div>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="btn btn-primary w-75 mx-auto d-block">
-              Asociate
-            </a>
-          </div>
-        </div>
-        <div className="card col-3">
-          <div className="card-body">
-            <h5 className="card-title fs-3">Plan Total</h5>
-            <hr />
-            <p className="card-text fs-5 text-center py-2">
-              $<span> 10.000</span>/mes
-            </p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-          </ul>
-          <div className="card-body">
-            <a href="#" className="btn btn-primary w-75 mx-auto d-block">
-              Asociate
-            </a>
-          </div>
-        </div>
       </div>
     </section>
   );
