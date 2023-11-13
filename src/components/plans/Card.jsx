84d87def9dc5e453d4ba5c096d0c2bec0ef5e57d
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-13 17:54:28 -0300

    Agrego Componente de Plan

--- src/components/plans/Card.jsx
+++ src/components/plans/Card.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Card = ({ plan }) => {
   return (
     <div className="col-12 col-md-6 col-lg-4 px-lg-2 m-3 mx-md-0">
       <div className="card-body bg-blue py-5 ">
         <h5 className="card-title fs-2  text-white mx-4">{plan.nombre}</h5>
         <hr className="border-orange " />
-        <p className="card-text fs-2 text-center py-3 text-title text-our-white">
+        <p className="card-text fs-2 text-center py-2 text-title text-our-white">
           $<span>{plan.precio}</span>/mes
         </p>
       </div>
@@ -19,9 +21,12 @@ const Card = ({ plan }) => {
         ))}
       </ul>
       <div className="card-body bg-blue py-3">
-        <a href="#" className="btn w-75 mx-auto d-block fw-bold">
+        <Link
+          to={`/plan-details/${plan._id}`}
+          className="btn w-75 mx-auto d-block fw-bold"
+        >
           Asociate
-        </a>
+        </Link>
       </div>
     </div>
   );
