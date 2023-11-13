8bcdae485660c2acdc07123a9a9ccaf73335e082
Lucia Paterlini <luciam.paterlini@gmail.com>
2023-11-13 21:33:53 +0100

    agregue nuestras imagenes a los iconos de los profes

--- src/components/trainers/Card.jsx
+++ src/components/trainers/Card.jsx
@@ -1,10 +1,11 @@
+import "../../css/trainers.css"
 const Card = ({ trainer }) => {
   return (
     <div className="col-10 col-md-5 col-lg-3 m-3 m-lg-0">
       <div className="card rounded-circle px-0" key={trainer._id}>
         <img
-          src={trainer.img}
-          className="card-img rounded-circle"
+          src="/assets/img/gonza-img.png"
+          className="card-img rounded-circle img-trainer"
           alt={`Imagen de Entrenador ${trainer.nombre}`}
         />
       </div>
