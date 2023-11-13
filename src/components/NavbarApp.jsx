717bd51acf75447d749d8db4780fcb4d488e71f6
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-13 05:10:08 -0300

    Actualizo links de navbar

--- src/components/NavbarApp.jsx
+++ src/components/NavbarApp.jsx
@@ -8,7 +8,7 @@ const NavbarApp = ({ user = "", login, cerrarSesion }) => {
 
   return (
     <nav className="bg-blue navbar-expand-lg">
-      <div className="container-fluid pb-3">
+      <div className="container pb-3">
         <header className=" d-flex flex-wrap align-items-center justify-content-center justify-content-lg-between py-4 border-bottom w-100 mx-lg-1">
           <div className=" col-auto mb-2 mb-md-0">
             <NavLink className="navbar-brand logo text-white fs-4" to="/">
@@ -31,7 +31,7 @@ const NavbarApp = ({ user = "", login, cerrarSesion }) => {
             <li className="nav-item">
               <NavLink
                 className="nav-link px-3"
-                to="/servicios"
+                to="/services"
                 style={({ isActive }) => (isActive ? activeLink : undefined)}
               >
                 Servicios
@@ -40,25 +40,7 @@ const NavbarApp = ({ user = "", login, cerrarSesion }) => {
             <li className="nav-item">
               <NavLink
                 className="nav-link px-3"
-                to="/planes"
-                style={({ isActive }) => (isActive ? activeLink : undefined)}
-              >
-                Planes
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-link px-3"
-                to="/nuestroEquipo"
-                style={({ isActive }) => (isActive ? activeLink : undefined)}
-              >
-                Nuestro Equipo
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-link px-3"
-                to="/contacto"
+                to="/contact"
                 style={({ isActive }) => (isActive ? activeLink : undefined)}
               >
                 Contacto
