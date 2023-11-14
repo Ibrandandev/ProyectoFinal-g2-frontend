deb6cf373d1a19a57aa11e5f94ca2593257f7ff0
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-14 13:19:31 -0300

    Agrego ruta GET de planes

--- src/helpers/plansApi.js
+++ src/helpers/plansApi.js
@@ -1,4 +1,19 @@
+const url = "http://localhost:8080/api/plans/";
 const url = "https://proyectofinal-g2-backend.onrender.com/api/plans/";
+
+export const getPlans = async () => {
+  try {
+    const resp = await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+    });
+
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getPlanById = async (id) => {
   try {
