4520a06b5cbd07663a482d1e4266aa03fed7a982
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-14 13:16:37 -0300

    Cambio respuesta en tabla de servicios

--- src/components/services/Table.jsx
+++ src/components/services/Table.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
-
+import { deleteService } from "../../helpers/servicesApi";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
-
-import "../../css/services.css";
-
 import ModalEdit from "./ModalEdit";
-
-import { deleteService } from "../../helpers/servicesApi";
 import FormCreate from "./FormCreate";
 
 const Table = ({ servicios, traerServicios }) => {
@@ -52,90 +47,89 @@ const Table = ({ servicios, traerServicios }) => {
       <div className="row">
         <div className="col">
           <div className="table-responsive">
-          <table className="table media">
-        <thead>
-          <tr>
-            <th scope="col">Nombre</th>
-            <th scope="col">Categoría</th>
-            <th scope="col">Profesor</th>
-            <th scope="col">Dias</th>
-            <th scope="col">Horario</th>
-            <th scope="col">Cupo</th>
-            <th scope="col">Estado </th>
-            <th></th>
-          </tr>
-        </thead>
+            <table className="table media text-center">
+              <thead>
+                <tr>
+                  <th scope="col">Nombre</th>
+                  <th scope="col">Categoría</th>
+                  <th scope="col">Profesor</th>
+                  <th scope="col">Dias</th>
+                  <th scope="col">Horario</th>
+                  <th scope="col">Cupo</th>
+                  <th scope="col">Estado </th>
+                  <th></th>
+                </tr>
+              </thead>
 
-        <tbody className="col-4">
-          {servicios.map((servicio) => (
-            <tr key={servicio._id}>
-              <th>{servicio.nombre}</th>
-              {/* <td>{servicio.categoria}</td> */}
-              <td>cardio</td>
-              {/* <td>{servicio.profesor}</td> */}
-              <td>gonzalo garcia</td>
-              <td className="d-flex flex-column h-100">
-                {servicio &&
-                  servicio.dias.map((dia) => (
-                    <p key={crypto.randomUUID()}>{dia}</p>
-                  ))}
-              </td>
-              <td>{servicio.horario}</td>
-              <td>{servicio.cupo}</td>
-              <td>
-                {servicio.estado ? (
-                  <i
-                    className="fa fa-check text-success d-flex justify-content-center"
-                    aria-hidden="true"
-                  ></i>
-                ) : (
-                  <i
-                    className="fa fa-times text-danger d-flex 
+              <tbody className="col-4">
+                {servicios.map((servicio) => (
+                  <tr key={servicio._id}>
+                    <th>{servicio.nombre}</th>
+                    <td>{servicio.categoria.nombre}</td>
+                    <td>
+                      {servicio.profesor.nombre} {servicio.profesor.apellido}
+                    </td>
+                    <td className="d-flex flex-column h-100">
+                      {servicio &&
+                        servicio.dias.map((dia) => (
+                          <p key={crypto.randomUUID()}>{dia}</p>
+                        ))}
+                    </td>
+                    <td>{servicio.horario}</td>
+                    <td>{servicio.cupo}</td>
+                    <td>
+                      {servicio.estado ? (
+                        <i
+                          className="fa fa-check text-success d-flex justify-content-center"
+                          aria-hidden="true"
+                        ></i>
+                      ) : (
+                        <i
+                          className="fa fa-times text-danger d-flex 
               justify-content-center "
-                    aria-hidden="true"
-                  ></i>
-                )}
-              </td>
-              <td>
-                <div className="d-flex gap-3">
-                  <button
-                    className="btn btn-warning btn-sm"
-                    onClick={() => handleShow(servicio._id)}
-                  >
-                    <i className="fa fa-pencil" aria-hidden="true"></i>
-                  </button>
+                          aria-hidden="true"
+                        ></i>
+                      )}
+                    </td>
+                    <td>
+                      <div className="d-flex gap-3">
+                        <button
+                          className="btn btn-warning btn-sm"
+                          onClick={() => handleShow(servicio._id)}
+                        >
+                          <i className="fa fa-pencil" aria-hidden="true"></i>
+                        </button>
 
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={() =>
-                      deshabilitarServicio(servicio.nombre, servicio._id)
-                    }
-                    disabled={!servicio.estado}
-                  >
-                    <i className="fa fa-trash" aria-hidden="true"></i>
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                        <button
+                          className="btn btn-danger btn-sm"
+                          onClick={() =>
+                            deshabilitarServicio(servicio.nombre, servicio._id)
+                          }
+                          disabled={!servicio.estado}
+                        >
+                          <i className="fa fa-trash" aria-hidden="true"></i>
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-        
 
-      <div className="col">
-      {show && (
-        <ModalEdit
-          show={show}
-          handleClose={handleClose}
-          serviceId={serviceId}
-        />
-      )}
-      </div>
-      
-      <div>
-        <FormCreate />
-      </div>
+          <div className="col">
+            {show && (
+              <ModalEdit
+                show={show}
+                handleClose={handleClose}
+                serviceId={serviceId}
+              />
+            )}
+          </div>
+
+          <div>
+            <FormCreate />
+          </div>
         </div>
       </div>
     </div>
